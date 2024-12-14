7f878072b6b0340c498cdbd836e8964c356a147a
Tereshchenko_KPP <Tereshchenko_KPP@TERESHCHENKO_KP>
2024-12-14 15:45:28 +0800

    Add project files.

---
+++ ManualDeployWithCloudDb/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ManualDeployWithCloudDb
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<Country> Countries {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = @"
+User Id=postgres.mhmyioplvfstphdlizsa;
+Password=Ubunta2020!;
+Server=aws-0-eu-central-1.pooler.supabase.com;
+Port=5432;
+Database=postgres
+";
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
---
+++ ManualDeployWithCloudDb/Country.cs
@@ -0,0 +1,12 @@
+﻿namespace ManualDeployWithCloudDb
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty;
+        public string Abbreviated { get; set; } = string.Empty;
+
+        public Country() { }
+    }
+}
